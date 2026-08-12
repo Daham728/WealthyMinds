da8cfd8f300eae6acbacf03a439a8e4487cff407
T.N.V. Perera <akumatekina96@gmail.com>
2026-08-12 14:12:34 +0530

    refactor(graph): add inDegree and outDegree vertex metrics in GraphNode (Member 2: T.N.V. Perera)

--- backend/src/com/wealthyminds/datastructures/GraphNode.java
+++ backend/src/com/wealthyminds/datastructures/GraphNode.java
@@ -4,14 +4,22 @@ public class GraphNode {
     private String id;
     private String label;
     private String type; // "ACCOUNT", "INCOME_SOURCE", "EXPENSE_CATEGORY", "LOAN", "INVESTMENT"
+    private int inDegree;
+    private int outDegree;
 
     public GraphNode(String id, String label, String type) {
         this.id = id;
         this.label = label;
         this.type = type;
+        this.inDegree = 0;
+        this.outDegree = 0;
     }
 
     public String getId() { return id; }
     public String getLabel() { return label; }
     public String getType() { return type; }
+    public int getInDegree() { return inDegree; }
+    public int getOutDegree() { return outDegree; }
+    public void incrementInDegree() { inDegree++; }
+    public void incrementOutDegree() { outDegree++; }
 }
